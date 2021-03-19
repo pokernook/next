bb083af5a004d1c3a25522d6c0dffa9af67060ca
Ben Walker <bwalke05@gmail.com>
2021-03-18 20:48:02 -0400

    Add graphql handler

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,6 +1,7 @@
-import Fastify from "fastify";
+import Fastify, { HTTPMethods } from "fastify";
 import helmet from "fastify-helmet";
 import mercurius from "mercurius";
+import { NextApiHandler } from "next";
 
 import { schema } from "../../schema";
 
@@ -12,4 +13,27 @@ const build = async () => {
     path: "/api/graphql",
     schema,
   });
+
+  return app;
 };
+
+const apiHandler: NextApiHandler = async (req, res) => {
+  const app = await build();
+  const { body, headers, statusCode } = await app.inject({
+    cookies: req.cookies,
+    headers: req.headers,
+    method: req.method as HTTPMethods,
+    payload: req.body,
+    query: req.query,
+    url: req.url,
+  });
+
+  for (const key in headers) {
+    const header = headers[key]!;
+    res.setHeader(key, header);
+  }
+
+  res.status(statusCode).end(body);
+};
+
+export default apiHandler;
