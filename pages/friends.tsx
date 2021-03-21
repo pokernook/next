70fb9ee9d3884bdf8af9650b568a907ea04c629b
Ben Walker <bwalke05@gmail.com>
2021-03-21 10:42:57 -0400

    Add mock friends page

---
+++ pages/friends.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+import { PrivateLayout } from "../components/PrivateLayout";
+
+const Friends: FC = () => {
+  return (
+    <PrivateLayout>
+      <></>
+    </PrivateLayout>
+  );
+};
+
+export default Friends;
