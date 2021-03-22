3845dc83664d4b0adc6d5fb16c95637b96d78766
Ben Walker <bwalke05@gmail.com>
2021-03-21 22:15:43 -0400

    Hoist user check to auth layout

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -2,15 +2,21 @@ import Image from "next/image";
 import { FC, ReactNode } from "react";
 import { Container } from "theme-ui";
 
+import { useUser } from "../hooks/use-user";
+
 type AuthLayoutProps = {
   children: ReactNode;
 };
 
 export const AuthLayout: FC<AuthLayoutProps> = ({
   children,
-}: AuthLayoutProps) => (
-  <Container sx={{ maxWidth: 325, pt: 20, textAlign: "center" }}>
-    <Image height={128} width={128} src="/logo.svg" />
-    {children}
-  </Container>
-);
+}: AuthLayoutProps) => {
+  useUser({ redirectTo: "/", redirectIfFound: true });
+
+  return (
+    <Container sx={{ maxWidth: 325, pt: 20, textAlign: "center" }}>
+      <Image height={128} width={128} src="/logo.svg" />
+      {children}
+    </Container>
+  );
+};
