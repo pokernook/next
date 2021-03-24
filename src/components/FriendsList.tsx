916d6b143f168e38ff2d49f917a9c8bc1aed7c3a
Ben Walker <bwalke05@gmail.com>
2021-03-23 21:11:24 -0400

    Add friends page

---
+++ src/components/FriendsList.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+import { Avatar, Box, Divider, Flex, Heading } from "theme-ui";
+
+import { useFriendshipsQuery, UserFieldsFragment } from "../graphql/types";
+import { useAvatarSrc } from "../hooks/use-avatar-src";
+
+export const FriendsList: FC = () => {
+  const [friendshipsQuery] = useFriendshipsQuery();
+
+  const { data } = friendshipsQuery;
+
+  return (
+    <>
+      {data?.me?.friendships.map((friendship) =>
+        friendship.users.map(
+          (friend) =>
+            data.me?.id !== friend.id && <FriendsListItem user={friend} />
+        )
+      )}
+    </>
+  );
+};
+
+type FriendsListItemProps = {
+  user: UserFieldsFragment;
+};
+
+const FriendsListItem: FC<FriendsListItemProps> = ({
+  user,
+}: FriendsListItemProps) => {
+  return (
+    <>
+      <Divider />
+      <Flex sx={{ p: 3, borderRadius: 4, ":hover": { bg: "muted" } }}>
+        <Avatar
+          src={useAvatarSrc(user)}
+          sx={{ width: 48, height: 48, mr: 3 }}
+        />
+        <Box>
+          <Flex mb={2}>
+            <Heading as="h3">{user.username}</Heading>
+            <Heading as="h3" sx={{ color: "textMuted", fontWeight: "body" }}>
+              {user.discriminator}
+            </Heading>
+          </Flex>
+
+          {user.status &&
+            `${user.status.emoji || ""} ${user.status.message || ""}`}
+        </Box>
+      </Flex>
+    </>
+  );
+};
--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
+import { Box, Container, Heading } from "theme-ui";
 
 import { DashboardLayout } from "../components/DashboardLayout";
+import { FriendsList } from "../components/FriendsList";
 
 const Friends: FC = () => (
   <DashboardLayout>
-    <></>
+    <Container sx={{ maxWidth: 900, pt: 20 }}>
+      <Heading as="h1" mb={4}>
+        Friends
+      </Heading>
+
+      <Box>
+        <FriendsList />
+      </Box>
+    </Container>
   </DashboardLayout>
 );
 
