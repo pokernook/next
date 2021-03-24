f56e922d3f77fd6f8f4f895137f04654d56b94b0
Ben Walker <bwalke05@gmail.com>
2021-03-23 21:32:56 -0400

    Move friends page to directory

--- src/pages/friends/index.tsx
+++ src/pages/friends/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
 import { Box, Container, Heading } from "theme-ui";
 
-import { DashboardLayout } from "../components/DashboardLayout";
-import { FriendsList } from "../components/FriendsList";
+import { DashboardLayout } from "../../components/DashboardLayout";
+import { FriendsList } from "../../components/FriendsList";
 
 const Friends: FC = () => (
   <DashboardLayout>
