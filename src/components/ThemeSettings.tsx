2a1ca2db5b24516c68d7c3f81ea71b2aeb3a7c14
Ben Walker <bwalke05@gmail.com>
2021-03-23 20:09:17 -0400

    Add theme settings

--- src/components/ThemeSettings.tsx
+++ src/components/ThemeSettings.tsx
@@ -1,8 +1,26 @@
-import { FC } from "react";
-import { useColorMode } from "theme-ui";
+import { ChangeEvent, FC } from "react";
+import { Box, Heading, Select, Text, useColorMode } from "theme-ui";
 
 export const ThemeSettings: FC = () => {
-  useColorMode();
+  const [colorMode, setColorMode] = useColorMode();
 
-  return <></>;
+  const handleModeChange = (e: ChangeEvent<HTMLSelectElement>) =>
+    setColorMode(e.target.value);
+
+  return (
+    <>
+      <Heading as="h3" mb={3}>
+        Theme
+      </Heading>
+
+      <Box mb={3}>
+        <Text>Choose how PokerNook looks to you.</Text>
+      </Box>
+
+      <Select defaultValue={colorMode} onChange={handleModeChange}>
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+      </Select>
+    </>
+  );
 };
