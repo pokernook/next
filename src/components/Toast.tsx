9b7e166dba2f9caa1cbebc543aab1bee3dbb47ff
Ben Walker <bwalke05@gmail.com>
2021-03-27 11:56:11 -0400

    Use theme ui alert for toasts

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,13 +1,18 @@
 import { FC, ReactNode } from "react";
+import { AppearanceTypes } from "react-toast-notifications";
+import { Alert } from "theme-ui";
 
 import { FadeIn } from "./Animated";
 
 type ToastProps = {
+  appearance: AppearanceTypes;
   children: ReactNode;
 };
 
-export const Toast: FC<ToastProps> = ({ children }: ToastProps) => (
+export const Toast: FC<ToastProps> = ({ appearance, children }: ToastProps) => (
   <FadeIn>
-    <div>{children}</div>
+    <Alert variant={appearance} sx={{ my: 2, minWidth: 300 }}>
+      {children}
+    </Alert>
   </FadeIn>
 );
