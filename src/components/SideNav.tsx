cd0c89d3b27f6310ac971d1dda37000aca6036de
Ben Walker <bwalke05@gmail.com>
2021-03-29 21:58:52 -0400

    Add icons to side nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,14 +1,25 @@
 import { FC } from "react";
+import { FiPlayCircle, FiUserPlus, FiUsers } from "react-icons/fi";
 import { Avatar, Box, Divider, Flex, Heading } from "theme-ui";
 
-import { NavLink, NavLinkProps } from "../components/NavLink";
+import { NavLink } from "../components/NavLink";
 import { useAvatarSrc } from "../hooks/use-avatar-src";
 import { useUser } from "../hooks/use-user";
 
-const sideNavRoutes: NavLinkProps[] = [
-  { href: "/", children: "Play" },
-  { href: "/friends", children: "Friends" },
-  { href: "/friends/pending", children: "Pending Friends" },
+type SideNavRouteProps = {
+  href: string;
+  icon: JSX.Element;
+  text: string;
+};
+
+const sideNavRoutes: SideNavRouteProps[] = [
+  { href: "/", icon: <FiPlayCircle size={18} />, text: "Play" },
+  { href: "/friends", icon: <FiUsers size={18} />, text: "Friends" },
+  {
+    href: "/friends/pending",
+    icon: <FiUserPlus size={18} />,
+    text: "Pending Friends",
+  },
 ];
 
 export const SideNav: FC = () => {
@@ -28,10 +39,11 @@ export const SideNav: FC = () => {
       <Divider my={3} />
 
       <Box as="nav" mx={3}>
-        {sideNavRoutes.map((routeProps, idx) => (
-          <Box key={idx} sx={{ my: 1 }}>
-            <NavLink {...routeProps} />
-          </Box>
+        {sideNavRoutes.map(({ href, text, icon }, idx) => (
+          <NavLink href={href} key={idx} sx={{ my: 1 }}>
+            <Flex mr={3}>{icon}</Flex>
+            {text}
+          </NavLink>
         ))}
       </Box>
     </>
