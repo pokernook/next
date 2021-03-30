16b0d494f001fb4e34cdcef87be9ef6a97c81328
Ben Walker <bwalke05@gmail.com>
2021-03-30 19:35:17 -0400

    Add nav to friends layout

--- src/components/FriendsLayout.tsx
+++ src/components/FriendsLayout.tsx
@@ -1,16 +1,35 @@
 import { FC, ReactNode } from "react";
-import { Container, Divider, Heading } from "theme-ui";
+import { Box, Container, Divider, Flex, Heading } from "theme-ui";
+
+import { NavLink, NavLinkProps } from "./NavLink";
 
 type FriendsLayoutProps = {
   children: ReactNode;
 };
 
+const friendsRoutes: NavLinkProps[] = [
+  { children: "All friends", href: "/friends" },
+  { children: "Pending requests", href: "/friends/pending" },
+];
+
 export const FriendsLayout: FC<FriendsLayoutProps> = ({
   children,
 }: FriendsLayoutProps) => (
   <Container sx={{ maxWidth: 900, pt: 20 }}>
-    <Heading as="h1">Friends</Heading>
+    <Heading as="h2" mb={2}>
+      Friends
+    </Heading>
+
+    <Flex>
+      {friendsRoutes.map((props, idx) => (
+        <Box key={idx} sx={{ mr: 2 }}>
+          <NavLink {...props} />
+        </Box>
+      ))}
+    </Flex>
+
     <Divider my={3} />
+
     {children}
   </Container>
 );
